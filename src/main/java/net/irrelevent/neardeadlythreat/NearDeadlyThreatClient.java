c2ca2e4f21ceac5bef9a2663c3b2c1a5728c5384
IrrelevantCube <86004805+IrrelevantCube@users.noreply.github.com>
2024-01-15 08:53:47 -0700

    Initial commit

---
+++ src/main/java/net/irrelevent/neardeadlythreat/NearDeadlyThreatClient.java
@@ -0,0 +1,10 @@
+package net.irrelevent.neardeadlythreat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NearDeadlyThreatClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/irrelevent/neardeadlythreat/NearDeadlyThreatDataGenerator.java
@@ -0,0 +1,11 @@
+package net.irrelevent.neardeadlythreat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NearDeadlyThreatDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
