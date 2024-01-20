e64c4b86a426c8a6803c10ab396ca168ebef3f87
IrrelevantCube <86004805+IrrelevantCube@users.noreply.github.com>
2024-01-19 17:41:08 -0700

    Loot tables :O

--- src/main/java/net/irrelevent/neardeadlythreat/block/ModBlocks.java
+++ src/main/java/net/irrelevent/neardeadlythreat/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.irrelevent.neardeadlythreat.NearDeadlyThreat;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -12,14 +13,15 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block DOMINITE_ORE = registerBlock ("dominite_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).instrument(Instrument.CHIME).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+            new ExperienceDroppingBlock (FabricBlockSettings.copyOf(Blocks.ANCIENT_DEBRIS).strength (3.5f, 2000).instrument(Instrument.CHIME).sounds(BlockSoundGroup.AMETHYST_BLOCK), UniformIntProvider.create (5, 8)));
     public static final Block DOMINITE_BLOCK = registerBlock ("dominite_block",
             new Block(FabricBlockSettings.copyOf(Blocks.GOLD_BLOCK).instrument(Instrument.CHIME).sounds (BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block CHRODOMACH_BLOCK = registerBlock ("chrodomach_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_BLOCK).instrument(Instrument.CHIME).luminance (12).sounds (BlockSoundGroup.ANVIL)));
+            new Block(FabricBlockSettings.copyOf(Blocks.NETHERITE_BLOCK).instrument(Instrument.CHIME).luminance (12).sounds (BlockSoundGroup.ANVIL)));
     private static Block registerBlock(String name, Block block) {
         registerBlockItem (name, block);
         return Registry.register (Registries.BLOCK, new Identifier (NearDeadlyThreat.MOD_ID, name), block);
